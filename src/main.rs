4ca922288447ea58154030adab2779853926be11
ninjasgithub <60543166+ninjasgithub@users.noreply.github.com>
2020-09-05 17:23:58 +0100

    default

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,9 @@ mod scanner;
 mod lexer_types;
 
 
-fn main() {
-	loop {
-		let code = input("> ").unwrap();
-		match code.as_str() {
-			"exit" => std::process::exit(0),
-			_ =>  {
-				let mut scanner = scanner::scanner::new(code);
-				scanner.scan_tokens();
-				println!("{:#?}", scanner.get_tokens());
-			}
-		}
-	}
-}
 
-fn input(prompt: &str) -> std::io::Result<String> {
-	let mut result: String = String::new();
-	print!("{}", prompt);
-	std::io::stdout().flush()?;
-	std::io::stdin().read_line(&mut result)?;
-	Ok(result.trim().to_string())
+fn main() {
+	let mut scanner = scanner::scanner::new("".to_string());
+	scanner.scan_tokens();
+	println!("{:#?}", scanner.get_tokens());
 }
