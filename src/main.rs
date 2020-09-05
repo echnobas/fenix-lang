cc948e611d5e42c4f64be890762bf373ceae20e9
ninjasgithub <60543166+ninjasgithub@users.noreply.github.com>
2020-09-05 17:49:07 +0100

    clear command

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,16 @@ mod lexer_types;
 
 fn main() {
 	loop {
-		let mut scanner = scanner::scanner::new(input("> ").unwrap());
-		scanner.scan_tokens();
-		println!("{:#?}", scanner.get_tokens());
+		let code: String = input("> ").unwrap();
+		match code.as_str() {
+			"exit" => { std::process::exit(0) },
+			"clear" => { std::process::Command::new("cmd.exe").args(&["/c", "cls"]).status().unwrap().success(); },
+			_ => {
+				let mut scanner = scanner::scanner::new(code);
+				scanner.scan_tokens();
+				println!("{:#?}", scanner.get_tokens());
+			}
+		}
 	}
 }
 
--- target/rls/debug/deps/rust_interpreter-b2cf4fbeafe892fb.d
+++
@@ -1,7 +0,0 @@
-c:\Users\Esquire\Desktop\rust_interpreter\target\rls\debug\deps\rust_interpreter-b2cf4fbeafe892fb.rmeta: src\main.rs src\scanner.rs src\lexer_types.rs
-
-c:\Users\Esquire\Desktop\rust_interpreter\target\rls\debug\deps\rust_interpreter-b2cf4fbeafe892fb.d: src\main.rs src\scanner.rs src\lexer_types.rs
-
-src\main.rs:
-src\scanner.rs:
-src\lexer_types.rs:
